ac9737a5e37677a23aac96081bcba6aece8ffd72
iberezhnyi <iberezhnyi@gmail.com>
2024-10-04 09:32:50 +0000

    Deploying to gh-pages from @ iberezhnyi/phonebook@e7a8b56ad9ee16247984bd9d7b8aa244d5050411 🚀

---
+++ static/js/544.243112e0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,r){r.r(t);var n=r(3329);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("p",{style:{textAlign:"center"},children:"Welcome to your Phonebook!"})})}}}]);
+//# sourceMappingURL=544.243112e0.chunk.js.map
