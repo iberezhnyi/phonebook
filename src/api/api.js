3795e21efd974043e88f9f57ee68796806222bcb
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-10-08 16:40:19 +0300

    fix(api): fix api

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 export const publicApi = axios.create({
+  // baseURL: 'http://localhost:3001/api',
   baseURL: 'https://phonebook-backend-k1p3.onrender.com/api',
 });
 
 export const privateApi = axios.create({
+  // baseURL: 'http://localhost:3001/api',
   baseURL: 'https://phonebook-backend-k1p3.onrender.com/api',
 });
 
